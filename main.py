dc11ed71510800b5b2d88940b3fe02f6a72086f4
Aviv Tahar <sh.tahar@gmail.com>
2021-11-01 19:58:16 +0200

    cr_fixes

--- main.py
+++ main.py
@@ -21,27 +21,26 @@ def collect_jobs(driver_collect):
     :return: The function stores the collected data into a list of dictionaries
     and prints the list
     """
-    # Acquire parent tags
+    # Acquire the list that encapsulates all the job entries
     title_divs = driver_collect.find_elements(By.CLASS_NAME,
                                               'base-search-card__info')
+
     # Acquire data for each individual parent tag
     list_of_dicts = []
     for div in title_divs:
         title = div.find_element(By.CLASS_NAME, 'base-search-card__title')
         company = div.find_element(By.CLASS_NAME, 'base-search-card__subtitle')
         location = div.find_element(By.CLASS_NAME, 'job-search-card__location')
-        divdate = div.find_element(By.TAG_NAME, 'time')
+        publish_period = div.find_element(By.TAG_NAME, 'time')
 
         list_of_dicts.append({'card_title': f'{title.text}',
                               'company': f'{company.text}',
                               'location': f'{location.text}',
-                              'date': f'{divdate.text}'})
+                              'date': f'{publish_period.text}'})
 
-    # Print each job dictionary
+    # Print each job entry
     for dictionary in list_of_dicts:
         print(dictionary)
-
-    driver_collect.close()
 
 
 if __name__ == '__main__':
@@ -59,3 +58,4 @@ if __name__ == '__main__':
     WebDriverWait(driver, TIMEOUT).until(element_present)
 
     collect_jobs(driver)
+    driver.close()
