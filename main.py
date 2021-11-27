4fd5c09efd258d851e8d71a8eb00eb9ea36363d0
Aviv Tahar <sh.tahar@gmail.com>
2021-11-27 23:16:11 +0200

    argparse for users input

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import selenium.common.exceptions as sel_exc
 from jobcollector import JobCollector
 from google_searcher import GoogleSearcher
 from configurations import *
+import argparse
 
 
 if __name__ == '__main__':
@@ -14,8 +15,16 @@ if __name__ == '__main__':
 
     # Initiate chrome driver and get website
     driver = webdriver.Chrome(options=chrome_options)
+
+    parser = argparse.ArgumentParser(description=
+                                     'Linkedin Job Scraping Project')
+    parser.add_argument(
+        'search_query', type=str,
+        help='User search string for a job: Enter job role and location')
+    args = parser.parse_args()
+
     google_searcher = GoogleSearcher(driver)
-    google_searcher.open_results(input('Enter google search: '))
+    google_searcher.open_results(args.search_query)
 
     # Collect Jobs
     jc = JobCollector(driver)
