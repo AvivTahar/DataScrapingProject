75b5d5c6a23ed150bf50debf251db3189e99d51c
Evgeny Oyffe <e.oyffe@gmail.com>
2021-12-13 21:37:11 +0200

    insert coordinates into db2

--- coord_fetcher.py
+++ coord_fetcher.py
@@ -4,7 +4,7 @@ from configurations import *
 
 class CoordFetcher:
     def __init__(self, access_key):
-        self.access_key = access_key
+        self.access_key = access_key[1:-1]
 
     def get_coord(self, city, country):
         response = requests.get(f'{GEO_API}forward?access_key={self.access_key}&query={city} {country}')
@@ -12,7 +12,3 @@ class CoordFetcher:
 
         return res_first['latitude'], res_first['longitude']
 
-
-if __name__ == '__main__':
-    cf = CoordFetcher('4306112a3088c5b22c4708e9915ccc2b')
-    cf.get_coord('moscow', 'russia')
