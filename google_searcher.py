75f6f5521ae0c87289d98011421eadb24c266be9
Evgeny Oyffe <e.oyffe@gmail.com>
2021-12-20 20:27:27 +0200

    try headless2

--- google_searcher.py
+++ google_searcher.py
@@ -29,9 +29,10 @@ class GoogleSearcher:
         logging.info('Clicking google search result performed')
 
         # Wait until first parsed tag is uploaded
-        element_present = ec.presence_of_element_located((
-            By.CLASS_NAME, 'base-search-card__info'))
-        WebDriverWait(self.driver, LIST_UPDATE_MAX_TIME).until(element_present)
+        sleep(WAIT_TIME)
+        # element_present = ec.presence_of_element_located((
+        #     By.CLASS_NAME, 'base-search-card__info'))
+        # WebDriverWait(self.driver, LIST_UPDATE_MAX_TIME).until(element_present)
         self.driver.maximize_window()
         logging.info('Chrome window has been maximized')
         sleep(WAIT_TIME)
